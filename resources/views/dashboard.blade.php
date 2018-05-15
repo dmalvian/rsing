954205d532e0008e413b223cf0ee8a10b2b317f9
Malvian Dwi <dmalvian@gmail.com>
2018-05-15 14:41:25 +0700

    Update Dashboard Carousel

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,41 @@
 @extends('base')
 
 @section('content')
-  <section class="hero is-medium is-primary">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title">
-        Primary title
-      </h1>
-      <h2 class="subtitle">
-        Primary subtitle
-      </h2>
+  <section class="hero is-medium is-primary has-carousel">
+    <div class="hero-carousel carousel-animated carousel-animate-fade" data-autoplay="true">
+      <div class='carousel-container'>
+        <div class='carousel-item has-background is-active'>
+          <img class="is-background" src="{{ asset('img/carousel-1.jpg') }}" alt="" />
+        </div>
+        <div class='carousel-item has-background'>
+          <img class="is-background" src="{{ asset('img/carousel-2.jpg') }}" alt="" />
+        </div>
+        <div class='carousel-item has-background'>
+          <img class="is-background" src="{{ asset('img/carousel-3.jpg') }}" alt="" />
+        </div>
+        <div class='carousel-item has-background'>
+          <img class="is-background" src="{{ asset('img/carousel-4.jpg') }}" alt="" />
+        </div>
+      </div>
+      <div class="carousel-navigation is-overlay">
+        <div class="carousel-nav-left">
+          <i class="fa fa-chevron-left" aria-hidden="true"></i>
+        </div>
+        <div class="carousel-nav-right">
+          <i class="fa fa-chevron-right" aria-hidden="true"></i>
+        </div>
+      </div>
     </div>
-  </div>
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Primary title
+        </h1>
+        <h2 class="subtitle">
+          Primary subtitle
+        </h2>
+      </div>
+    </div>
   </section>
 
   <section class="has-background-white-ter">
@@ -29,4 +53,10 @@
       </div>
     </div>
   </footer>
+
+  <script> 
+      document.addEventListener('DOMContentLoaded', function(){
+        var carousels = bulmaCarousel.attach();
+      }, false);
+    </script>
 @endsection
